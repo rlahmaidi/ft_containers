c095309bb0d04a325636a038c41cb681013f5925
Rachid Lahmaidi El Idrissi <rlahmaid@e2r3p2.1337.ma>
2022-11-03 17:24:05 +0100

    ddf

---
+++ tests/pair_test.cpp
@@ -0,0 +1,17 @@
+#include <utility>      // std::pair
+#include "../pair.hpp"    
+
+int main ()
+{
+  ft::pair<int,char> foo (10,'z');
+  ft::pair<int,char> bar (90,'a');
+
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
+  return 0;
+}
